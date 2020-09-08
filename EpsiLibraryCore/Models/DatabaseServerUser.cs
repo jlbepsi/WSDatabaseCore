1327403e4843dbecfd2d0b7449f092ee66ea4003
James Kilroy <james.kilroy@gmail.com>
2020-09-08 16:23:12 +0200

    Ajout des Modèles SQL Server dans EpsiLibraryCore

---
+++ EpsiLibraryCore/Models/DatabaseServerUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EpsiLibraryCore.Models
+{
+    public partial class DatabaseServerUser
+    {
+        public int ServerId { get; set; }
+        public string SqlLogin { get; set; }
+        public string UserLogin { get; set; }
+
+        public virtual DatabaseServerName Server { get; set; }
+    }
+}
---
+++ EpsiLibraryCore/Models/ServerAccountModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsiLibraryCore.Models
+{
+    public class ServerAccountModel
+    {
+        public int ServerId { get; set; }
+        public string UserLogin { get; set; }
+        public string Password { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("ServerId={0}, UserLogin={1}", ServerId, UserLogin);
+        }
+    }
+}
